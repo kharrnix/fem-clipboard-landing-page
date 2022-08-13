f1c2d66c6c51a5130d7c5ce12368504dd2c2f369
kharrnix <93736879+kharrnix@users.noreply.github.com>
2022-08-13 19:02:01 -0400

    Add class names to SectionFour.js

--- src/components/SectionFour.js
+++ src/components/SectionFour.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./SectionFour.css"
+import "./SectionFour.css";
 
 import iconBlacklist from "../images/icon-blacklist.svg";
 import iconText from "../images/icon-text.svg";
@@ -8,25 +8,31 @@ import iconPreview from "../images/icon-preview.svg";
 export default function SectionFour() {
 	return (
 		<>
-			<section>
-				<h2>Supercharge your workflow</h2>
-				<p>We've got the tools to boost your productivity.</p>
+			<section className="section4">
+				<h2 className="section4__heading">Supercharge your workflow</h2>
+				<p className="section4__main-paragraph">
+					We've got the tools to boost your productivity.
+				</p>
 
-				<img src={iconBlacklist} alt="" />
-				<h3>Create blacklists</h3>
-				<p>
+				<img className="section4__icon" src={iconBlacklist} alt="" />
+				<h3 className="section4__secondary-heading">
+					Create blacklists
+				</h3>
+				<p className="section4__paragraph">
 					Ensure sensitive information never makes its way to your
 					clipboard by excluding certain sources.
 				</p>
-				<img src={iconText} alt="" />
-				<h3>Plain text snippets</h3>
-				<p>
+				<img className="section4__icon" src={iconText} alt="" />
+				<h3 className="section4__secondary-heading">
+					Plain text snippets
+				</h3>
+				<p className="section4__paragraph">
 					Remove unwanted formatting from copied text for a consistent
 					look.
 				</p>
-				<img src={iconPreview} alt="" />
-				<h3>Sneak preview</h3>
-				<p>
+				<img className="section4__icon" src={iconPreview} alt="" />
+				<h3 className="section4__secondary-heading">Sneak preview</h3>
+				<p className="section4__paragraph">
 					Quick preview of all snippets on your Clipboard for easy
 					access.
 				</p>
