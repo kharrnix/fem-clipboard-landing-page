cc0a7dcc6394042d3e140cad57168d05119e3ea5
kharrnix <93736879+kharrnix@users.noreply.github.com>
2022-08-14 18:11:52 -0400

    Add class names to SectionSix.js and import button components

--- src/components/SectionSix.js
+++ src/components/SectionSix.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./SectionSix.css";
+import LinkButtonPrimary from "./LinkButtonPrimary";
+import LinkButtonSecondary from "./LinkButtonSecondary";
 
 export default function SectionSix() {
 	return (
 		<>
-			<section>
-				<h2>Clipboard for iOS and Mac OS</h2>
-				<p>
+			<section className="section6">
+				<h2 className="section6__heading">Clipboard for iOS and Mac OS</h2>
+				<p className="section6__paragraph">
 					Available for free on the App Store. Download for Mac or
 					iOS, sync with iCloud and you’re ready to start adding to
 					your clipboard.
 				</p>
-				<a href="#">Download for iOS</a>
-				<a href="#">Download for Mac</a>
+
+				<LinkButtonPrimary link="/download" text="Download for iOS"/>
+				<LinkButtonSecondary link="/download" text="Download for Mac"/>
 			</section>
 		</>
 	);
