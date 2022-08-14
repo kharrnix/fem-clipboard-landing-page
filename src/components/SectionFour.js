947ee51c53b6b1e8b7f4118a7ce689d93d81f948
kharrnix <93736879+kharrnix@users.noreply.github.com>
2022-08-14 17:35:05 -0400

    Add grid to fourth section for larger screens

--- src/components/SectionFour.js
+++ src/components/SectionFour.js
@@ -14,28 +14,46 @@ export default function SectionFour() {
 					We've got the tools to boost your productivity.
 				</p>
 
-				<img className="section4__icon" src={iconBlacklist} alt="" />
-				<h3 className="section4__secondary-heading">
-					Create blacklists
-				</h3>
-				<p className="section4__paragraph">
-					Ensure sensitive information never makes its way to your
-					clipboard by excluding certain sources.
-				</p>
-				<img className="section4__icon" src={iconText} alt="" />
-				<h3 className="section4__secondary-heading">
-					Plain text snippets
-				</h3>
-				<p className="section4__paragraph">
-					Remove unwanted formatting from copied text for a consistent
-					look.
-				</p>
-				<img className="section4__icon" src={iconPreview} alt="" />
-				<h3 className="section4__secondary-heading">Sneak preview</h3>
-				<p className="section4__paragraph">
-					Quick preview of all snippets on your Clipboard for easy
-					access.
-				</p>
+				<div className="section4__grid-container">
+					<div className="section4__column">
+						<img
+							className="section4__icon"
+							src={iconBlacklist}
+							alt=""
+						/>
+						<h3 className="section4__secondary-heading">
+							Create blacklists
+						</h3>
+						<p className="section4__paragraph">
+							Ensure sensitive information never makes its way to
+							your clipboard by excluding certain sources.
+						</p>
+					</div>
+					<div className="section4__column">
+						<img className="section4__icon" src={iconText} alt="" />
+						<h3 className="section4__secondary-heading">
+							Plain text snippets
+						</h3>
+						<p className="section4__paragraph">
+							Remove unwanted formatting from copied text for a
+							consistent look.
+						</p>
+					</div>
+					<div className="section4__column">
+						<img
+							className="section4__icon"
+							src={iconPreview}
+							alt=""
+						/>
+						<h3 className="section4__secondary-heading">
+							Sneak preview
+						</h3>
+						<p className="section4__paragraph">
+							Quick preview of all snippets on your Clipboard for
+							easy access.
+						</p>
+					</div>
+				</div>
 			</section>
 		</>
 	);
