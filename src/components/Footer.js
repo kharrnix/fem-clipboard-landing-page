ea598b216bf0d29e63cae3e25c432c35898d37fc
kharrnix <93736879+kharrnix@users.noreply.github.com>
2022-08-17 12:34:59 -0400

    Style footer for large screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,40 +10,46 @@ export default function Footer() {
 		<>
 			<footer className="footer">
 				<nav className="footer__nav">
-					<a href="index.html">
-						<img className="footer__logo" src={iconLogo} alt="" />
-					</a>
-					<ul className="footer__column">
-						<li>
-							<a href="/faq" className="footer__link">
-								FAQs
-							</a>
-						</li>
-						<li>
-							<a href="/contact" className="footer__link">
-								Contact Us
-							</a>
-						</li>
-					</ul>
-					<ul className="footer__column">
-						<li>
-							<a href="/privacy" className="footer__link">
-								Privacy Policy
-							</a>
-						</li>
-						<li>
-							<a href="/press-kit" className="footer__link">
-								Press Kit
-							</a>
-						</li>
-					</ul>
-					<ul className="footer__column">
-						<li>
-							<a href="/install" className="footer__link">
-								Install Guide
-							</a>
-						</li>
-					</ul>
+					<div className="footer__left">
+						<a href="index.html">
+							<img
+								className="footer__logo"
+								src={iconLogo}
+								alt=""
+							/>
+						</a>
+						<ul className="footer__column">
+							<li>
+								<a href="/faq" className="footer__link">
+									FAQs
+								</a>
+							</li>
+							<li>
+								<a href="/contact" className="footer__link">
+									Contact Us
+								</a>
+							</li>
+						</ul>
+						<ul className="footer__column">
+							<li>
+								<a href="/privacy" className="footer__link">
+									Privacy Policy
+								</a>
+							</li>
+							<li>
+								<a href="/press-kit" className="footer__link">
+									Press Kit
+								</a>
+							</li>
+						</ul>
+						<ul className="footer__column">
+							<li>
+								<a href="/install" className="footer__link">
+									Install Guide
+								</a>
+							</li>
+						</ul>
+					</div>
 					<ul className="footer__column footer__social-list">
 						<li>
 							<a
